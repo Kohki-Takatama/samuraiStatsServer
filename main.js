c5898650937158bac15173c7330503a7bceeddb6
Kohki-Takatama <koh.takatama@gmail.com>
2023-06-05 12:57:41 +0900

    fix

--- main.js
+++ main.js
@@ -6,15 +6,28 @@ const updateDbWithScrape = utilities.updateDbWithScrape;
 const receiveAndPassData = (event) => {
   const msg = event.message.text;
   const token = event.replyToken;
-  if (msg.includes("set")) {
-    updateDbWithScrape(token, samuraiList);
-  } else if (msg.includes("recent")) {
-    replyToLine.recentStats(token);
-  } else if (msg.includes("total")) {
-    replyToLine.totalStats(token);
-  } else {
-    replyToLine.noKeyword(token);
+  switch (msg) {
+    case "set":
+      updateDbWithScrape(token, samuraiList);
+      break;
+    case "recent":
+      replyToLine.recentStats(token);
+      break;
+    case "total":
+      replyToLine.totalStats(token);
+      break;
+    default:
+      replyToLine.noKeyword(token);
   }
+  // if (msg.includes("set")) {
+  //   updateDbWithScrape(token, samuraiList);
+  // } else if (msg.includes("recent")) {
+  //   replyToLine.recentStats(token);
+  // } else if (msg.includes("total")) {
+  //   replyToLine.totalStats(token);
+  // } else {
+  //   replyToLine.noKeyword(token);
+  // }
 };
 
 module.exports = receiveAndPassData;

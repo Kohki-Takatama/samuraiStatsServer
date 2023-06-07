8aabd5d140fca63802f3cacaf3306d9d88aa83d3
Kohki-Takatama <koh.takatama@gmail.com>
2023-06-07 14:12:57 +0900

    fix

--- scrape.js
+++ scrape.js
@@ -55,11 +55,12 @@ const mergeArraysToDictionary = (keyArray, dataArray) => {
 };
 
 const scrapeAndSaveToDb = async (parameter) => {
-  const html = await returnHTML(url);
   const url = parameter.url;
   const selectors = parameter.selector;
   const name = parameter.name;
   console.log(`run: updateDbWithScrapeData, doing: scrape: ${name}`);
+
+  const html = await returnHTML(url);
   let returnScrapeArray = {};
   try {
     //NOTE: 名前

310828025842ebe06c1aebbe9cd8f7bad91d56e1
Kohki-Takatama <koh.takatama@gmail.com>
2023-06-22 11:19:48 +0900

    fix: replymsgを自然な日本語に修正

--- utilitieFunctions.js
+++ utilitieFunctions.js
@@ -32,9 +32,11 @@ const formatToReply = {
         nowOrNot ? "しています" : "しました"
       }。\n${scrapedData.recentStats.投球回}回を投げ、失点は${scrapedData.recentStats.失点}、自責点は${
         scrapedData.recentStats.自責点
-      } で${nowOrNot ? "す" : "した"}。\n勝敗は${
-        scrapedData.recentStats.結果 === "-" ? "つきません" : scrapedData.recentStats.結果
-      }でした。\n詳細な成績→ 被安打:${scrapedData.recentStats.被安打} 被本塁打:${
+      } で、${
+        scrapedData.recentStats.結果 === "-"
+          ? "勝/敗はついていません。"
+          : scrapedData.recentStats.結果 + "投手となっています。"
+      }\n詳細な成績→ 被安打:${scrapedData.recentStats.被安打} 被本塁打:${
         scrapedData.recentStats.被本塁打
       } 奪三振:${scrapedData.recentStats.奪三振} 与四死球:${
         Number(scrapedData.recentStats.与四球) + Number(scrapedData.recentStats.与死球)
